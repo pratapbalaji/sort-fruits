49e01a3fdcfa7989982c8ec1a590637db8d08890
Vinay Balaji <vinaybalaji@gmail.com>
2018-10-14 18:36:55 -0700

    Moved the last case outside of the second loop as this condition can be checked before

--- sort-fruits.js
+++ sort-fruits.js
@@ -24,6 +24,8 @@ var newlist = []
 for(var i = 0; i < list.length; i++) {
   if (newlist.length === 0) {
     newlist.push(list[0])
+  } else if (list[i].qty > newlist[newlist.length-1].qty) {
+    newlist.push(list[i])
   } else {
     for (var j = 0; j < newlist.length; j++) {
       if (list[i].qty < newlist[j].qty) {
@@ -31,9 +33,6 @@ for(var i = 0; i < list.length; i++) {
         break;
       } else if (list[i].qty > newlist[j].qty && list[i].qty <= newlist[j+1].qty) {
         newlist.splice(j+1, 0, list[i])
-        break;
-      } else if (list[i].qty > newlist[newlist.length-1].qty) {
-        newlist.push(list[i])
         break;
       }
     }
